6be0d499b3685209fd6b4c7a2913fb6fe8cb5a23
AdrianLutu <lutu.adrian.catalin@gmail.com>
2026-02-06 19:30:25 +0200

    Refactored bert baseline

--- baselines/finetune_bert/finetune_bert.py
+++ baselines/finetune_bert/finetune_bert.py
@@ -43,20 +43,17 @@ from tqdm import tqdm
 
 
 import numpy as np
-from sklearn.manifold import TSNE
-from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
-from sklearn.metrics import silhouette_score
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-df_train = pd.read_csv("/home/eduard/Desktop/Research/Adrian/VQPP/GRAM/GRAM/metrics/msrvtt_train.csv")
-df_eval = pd.read_csv("/home/eduard/Desktop/Research/Adrian/VQPP/GRAM/GRAM/metrics/msrvtt_val.csv")
-df_test = pd.read_csv("/home/eduard/Desktop/Research/Adrian/VQPP/GRAM/GRAM/metrics/msrvtt_test.csv")
+METRIC = "Reciprocal_Rank"
+
+df_train = pd.read_csv("/root/VQPP/resources/GRAM/metrics/vatex_train.csv")
+df_eval = pd.read_csv("/root/VQPP/resources/GRAM/metrics/vatex_val.csv")
+df_test = pd.read_csv("/root/VQPP/resources/GRAM/metrics/vatex_test.csv")
 
 
-df_train = df_train[["Query", "Recall@10"]]
-df_eval = df_eval[["Query", "Recall@10"]]
-df_test = df_test[["Query", "Recall@10"]]
+df_train = df_train[["Query", METRIC]]
+df_eval = df_eval[["Query", METRIC]]
+df_test = df_test[["Query", METRIC]]
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
 
@@ -68,7 +65,7 @@ def tokenize_and_prepare(dataframe):
         truncation=True,
         return_tensors="pt",
     )
-    labels = torch.tensor(dataframe["Recall@10"].values).unsqueeze(-1).float()
+    labels = torch.tensor(dataframe[METRIC].values).unsqueeze(-1).float()
     return inputs["input_ids"], inputs["attention_mask"], labels
 
 
@@ -76,25 +73,31 @@ train_inputs, train_masks, train_labels = tokenize_and_prepare(df_train)
 eval_inputs, eval_masks, eval_labels = tokenize_and_prepare(df_eval)
 test_inputs, test_masks, test_labels = tokenize_and_prepare(df_test)
 
-test_scores_list = df_test["Recall@10"].tolist()
+test_scores_list = df_test[METRIC].tolist()
 
-# +
 train_dataset = TensorDataset(train_inputs, train_masks, train_labels)
 eval_dataset = TensorDataset(eval_inputs, eval_masks, eval_labels)
 test_dataset = TensorDataset(test_inputs, test_masks, test_labels)
 
-train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True, pin_memory=True)
-eval_loader = DataLoader(eval_dataset, batch_size=256, pin_memory=True)
-test_loader = DataLoader(test_dataset, batch_size=256, pin_memory=True)
-
-
-# -
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, pin_memory=True)
+eval_loader = DataLoader(eval_dataset, batch_size=32, pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=32, pin_memory=True)
 
 
 class BertRegressor(nn.Module):
     def __init__(self, n_outputs=1):
         super(BertRegressor, self).__init__()
         self.bert = BertModel.from_pretrained("bert-base-cased")
+
+        for param in self.bert.parameters():
+            param.requires_grad = False
+            
+        for param in self.bert.encoder.layer[-1].parameters():
+            param.requires_grad = True
+            
+        for param in self.bert.pooler.parameters():
+            param.requires_grad = True
+
         self.drop = nn.Dropout(p=0.3)
         self.linear1 = nn.Linear(self.bert.config.hidden_size, 512)
         self.linear2 = nn.Linear(512, n_outputs)
@@ -154,14 +157,11 @@ best_params = {}
 for lr in param_grid["learning_rate"]:
     for epochs in param_grid["num_epochs"]:
         for decay in param_grid["weight_decay"]:
-            # Initialize model
             model = BertRegressor()
             model.to(device)
 
-            # Initialize optimizer with current hyperparameters
             optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=decay)
 
-            # Training loop
             for epoch in range(epochs):
                 model.train()
                 for i, batch in tqdm(enumerate(train_loader), total=len(train_loader),desc=f"Epoch {epoch}"):
@@ -172,13 +172,11 @@ for lr in param_grid["learning_rate"]:
                     loss.backward()
                     optimizer.step()
 
-            # Evaluation
             mse_eval, r_squared_eval = evaluate_model(model, eval_loader)
             print(
                 f"LR: {lr}, Epochs: {epochs}, Weight Decay: {decay}, Eval MSE: {mse_eval}, Eval R-squared: {r_squared_eval}"
             )
 
-            # Update best model if current model is better
             if mse_eval < best_mse:
                 best_mse = mse_eval
                 best_params = {
@@ -186,13 +184,10 @@ for lr in param_grid["learning_rate"]:
                     "num_epochs": epochs,
                     "weight_decay": decay,
                 }
-                # Save the best model
+
                 torch.save(model.state_dict(), "best_model.pth")
 
-# Print best parameters
 print(f"Best Parameters: {best_params}")
-
-# -
 
 model = BertRegressor()
 model.load_state_dict(torch.load("best_model.pth"))
@@ -203,8 +198,6 @@ mse_test, r_squared_test = evaluate_model(model, test_loader)
 print(f"Test MSE: {mse_test}")
 print(f"Test R-squared: {r_squared_test}")
 
-
-# +
 model.eval()
 all_predictions = []
 
@@ -213,7 +206,6 @@ with torch.no_grad():
         input_ids, attention_mask, _ = [b.to(device) for b in batch]
         outputs = model(input_ids, attention_mask=attention_mask)
         all_predictions.extend(outputs.cpu().numpy())
-# -
 
 predictions_scores = [float(pred) for pred in all_predictions]
 
@@ -224,14 +216,11 @@ predictions_df.to_csv(predictions_csv_path, index=False)
 
 
 def calculate_correlations(list1, list2):
-    # Check if the lists are of the same length
     if len(list1) != len(list2):
         return "The lists are not of the same length"
 
-    # Calculate Pearson correlation
     pearson_corr, pvaluep = scipy.stats.pearsonr(list1, list2)
 
-    # Calculate Kendall Tau correlation
     kendall_corr, pvalue = scipy.stats.kendalltau(list1, list2)
 
     return pearson_corr, pvaluep, kendall_corr, pvalue
